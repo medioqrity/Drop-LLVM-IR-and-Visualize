ad7cbc93a30fd451801033934e3c47a835f430d2
med <medioqrity@gmail.com>
2023-03-19 04:49:42 -0400

    remove electron; switch to simple nodejs server

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var node_path_1 = __importDefault(require("node:path"));
-var create_window = function () {
-    var win = new electron_1.BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: node_path_1.default.join(__dirname, "preload.js")
-        }
-    });
-    win.loadFile("index.html");
-};
-electron_1.app.whenReady().then(function () {
-    create_window();
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+            if (err) throw err;
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "text/html");
+            res.end(data);
+        });
+    }
+    if (req.url.endsWith(".js")) {
+        fs.readFile(path.join(__dirname, req.url.substring(1)), (err, data) => {
+            if (err) throw err;
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(data);
+        });
+    }
+    if (req.url === "/gen_dot") {
+        
+    }
 });
-electron_1.app.on("window-all-closed", function () {
-    electron_1.app.quit();
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
